e51e8dc7d44cc8f23b220d76c80fcae4e81b982f
nat5_776 <deindall@outlook.com>
2025-05-31 19:29:47 +0200

    port fixes (that for some reason didn't get included in merge)

--- src/System/Random.cpp
+++ src/System/Random.cpp
@@ -32,8 +32,8 @@ void Random::Seed64(unsigned long long lcg_state) {
 }
 
 void Random::Seed(unsigned long long lcg_state) {
-    static const unsigned long long multiplier = 0x5D588B656C078965uLL;
-    static const unsigned long long increment = 0x0000000000269EC3uLL;
+    unsigned long long multiplier = 0x5D588B656C078965uLL;
+    unsigned long long increment = 0x0000000000269EC3uLL;
 
     this->lcg_state = lcg_state;
     this->lcg_multiplier = multiplier;
